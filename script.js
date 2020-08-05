ac5a7de4f352e6ebb673d288b330adfc7b46eaa4
Shaumik <shaumikrahman123@gmail.com>
2020-08-05 15:13:28 +0100

    finished base game

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const diff = document.title;
 let guessCount;
 let ans;
+let first, last;
 
 if (diff == 'Easy') {
     easySpawn();
@@ -19,13 +20,23 @@ function rand(min, max) {
     return Math.floor(Math.random() * (max - min)) + min; 
   }
 
-function resetGame() {
+function newNumber() {
+    if (diff == 'Easy') {
+        ans = rand(1,11);
+    } else if ( diff == 'Medium') {
+        ans = rand(1,101);
+    } else {
+        ans = rand(1,1001);
+    }
+}
+
+function resetCounter() {
     guessCount = 0;
     document.getElementById('guessCount').innerHTML = 
     `Guesses : ${guessCount}`;
 }
 
-function updateGame() {
+function updateCounter() {
     document.getElementById('guessCount').innerHTML = 
     `Guesses : ${guessCount += 1}`;
 }
@@ -40,9 +51,56 @@ function easySpawn() {
             click(e.target.id);
         });
     }
-    resetGame();
+    resetCounter();
+    document.getElementById('again').hidden = true;
+    first = 1;
+    last = 10;
+}
+
+function numberIsCorrect(x) {
+    if (x == ans) {return true}
+    else return false;
+}
+
+function endGame(z) {
+    document.getElementById('buttonBox').innerHTML = 
+        `<button class="btn btn-primary" id="${z}">${z}</button>`;
+    document.getElementById('again').hidden = false;
+    document.getElementById('text').innerHTML = 'Correct';
+}
+
+function finalCheck(z) {
+    if (document.getElementById('buttonBox').childNodes.length == 2) {
+        endGame(z);
+    }
+}
+
+function continueGame(z) {
+    if (ans < z) {
+        for (let i = z; z<= last; z++) {
+            document.getElementById(`${z}`).remove();
+        }
+        document.getElementById('text').innerHTML = 'Lower';
+    } else {
+        for (let i = z; z>= first; z--) {
+            document.getElementById(`${z}`).remove();
+        }
+        document.getElementById('text').innerHTML = 'Higher';
+    }
 }
 
 function click(i) {
-    updateGame();
+    updateCounter();
+    if (numberIsCorrect(i)) {
+        endGame(i);
+    } else {
+        continueGame(i);
+        finalCheck(i);
+    }
 }
+
+console.log(ans);
+
+document.getElementById('again').addEventListener('click', (e) => {
+    location.reload();
+});
