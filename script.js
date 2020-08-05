d28f68898a7687aac0682795020c26d9bb099c71
Shaumik <shaumikrahman123@gmail.com>
2020-08-05 16:38:06 +0100

    polished base game

--- script.js
+++ script.js
@@ -62,44 +62,48 @@ function numberIsCorrect(x) {
     else return false;
 }
 
-function endGame(z) {
+function endGame() {
     document.getElementById('buttonBox').innerHTML = 
-        `<button class="btn btn-primary" id="${z}">${z}</button>`;
+        `<button class="btn btn-primary" id="${ans}">${ans}</button>`;
     document.getElementById('again').hidden = false;
     document.getElementById('text').innerHTML = 'Correct';
 }
 
-function finalCheck(z) {
-    if (document.getElementById('buttonBox').childNodes.length == 2) {
-        endGame(z);
+function continueGame(z) {
+    let click = z;
+    if (click > ans) {
+        for (let i = click; i <= last; i++) {
+            document.getElementById(`${i}`).remove();
+        }
+        last = click - 1
+        document.getElementById('text').innerHTML = 'Lower';
+    } else {
+        for (let i = click; i >= first; i--) {
+            document.getElementById(`${i}`).remove();
+        }
+        first = click + 1
+        document.getElementById('text').innerHTML = 'Higher';
     }
 }
 
-function continueGame(z) {
-    if (ans < z) {
-        for (let i = z; z<= last; z++) {
-            document.getElementById(`${z}`).remove();
-        }
-        document.getElementById('text').innerHTML = 'Lower';
-    } else {
-        for (let i = z; z>= first; z--) {
-            document.getElementById(`${z}`).remove();
-        }
-        document.getElementById('text').innerHTML = 'Higher';
+function finalCheck() {
+    if (document.getElementById('buttonBox').childElementCount == 1) {
+        updateCounter();
+        endGame();
     }
 }
 
 function click(i) {
     updateCounter();
     if (numberIsCorrect(i)) {
-        endGame(i);
+        endGame();
     } else {
         continueGame(i);
-        finalCheck(i);
+        finalCheck();
     }
 }
 
-console.log(ans);
+//console.log(ans);
 
 document.getElementById('again').addEventListener('click', (e) => {
     location.reload();
